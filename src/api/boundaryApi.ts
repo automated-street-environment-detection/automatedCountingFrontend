0583545fb38936784942386dee6402cead2564df
Fuitsuku <mitsulearnscode@gmail.com>
2024-09-30 16:42:07 -0700

    Generated apiClient object for all api classifications

--- src/api/boundaryApi.ts
+++ src/api/boundaryApi.ts
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+const API_BASE_LINK = "https://h50gco47p0.execute-api.us-east-2.amazonaws.com/dev"
+
+const apiClient = axios.create({
+    baseURL: API_BASE_LINK,
+    headers: {
+        "Content-Type": "application/json"
+    },
+});
