5e6f0f73da3715f3245daacde55a3526814fdb94
Fuitsuku <mitsulearnscode@gmail.com>
2024-10-01 16:07:39 -0700

    Removed different method calling, seems to break the button

--- src/ChosenVideoPage/ChosenVideoPage.jsx
+++ src/ChosenVideoPage/ChosenVideoPage.jsx
@@ -47,7 +47,7 @@ const ChosenVideoPage = () => {
         <button>filter</button>
         <button>upload</button>
         <button>work on local</button>
-        <button oncClick={() => testAPI()}>test api call</button>
+        <button oncClick={testAPI}>test api call</button>
       </div>
       <div style={{ marginBottom: "20px" }}>
         <input
