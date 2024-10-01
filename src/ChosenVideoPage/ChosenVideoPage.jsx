ff5753b6f19810888f9e547f99a1b105ac3f207a
Fuitsuku <mitsulearnscode@gmail.com>
2024-10-01 15:59:14 -0700

    Fixed syntax for ReactJS applications

--- src/ChosenVideoPage/ChosenVideoPage.jsx
+++ src/ChosenVideoPage/ChosenVideoPage.jsx
@@ -39,7 +39,7 @@ const ChosenVideoPage = () => {
         <button>filter</button>
         <button>upload</button>
         <button>work on local</button>
-        <button>test api call</button>
+        <button oncClick={getBucketId()}>test api call</button>
       </div>
       <div style={{ marginBottom: "20px" }}>
         <input
