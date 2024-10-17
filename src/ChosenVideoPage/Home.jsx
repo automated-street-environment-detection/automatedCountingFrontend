5b739d876a8f9b828417da7343e2de12da5aaa16
SishirVallurupalli <92895504+SishirVallurupalli@users.noreply.github.com>
2024-10-16 20:13:58 -0700

    Fixed issue with boudaries apearing twice

--- src/ChosenVideoPage/Home.jsx
+++ src/ChosenVideoPage/Home.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 import ChosenCountsPage from "../ChosenCountsPage/ChosenCountsPage";
 import { getVideoNames } from "../api/videoApi";
 import ChosenVideoPage from "./ChosenVideoPage";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { setBoundaryList } from "../redux/playerSlice";
 
 const Home = () => {
   const [videoList, setVideoList] = useState([]);
 
   const signedIn = useSelector((state) => state.signIn.loggedIn);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   useEffect(() => {
     const awaitNames = async () => {
       const response = await getVideoNames();
@@ -23,6 +25,7 @@ const Home = () => {
       }
     };
     if (signedIn) {
+      dispatch(setBoundaryList([]));
       awaitNames();
     } else {
       navigate("/");
